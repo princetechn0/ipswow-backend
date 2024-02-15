83d9fd6c6d0f9f82585da081cf17c8315f7ddb5a
princetechn0 <appleluvver8@gmail.com>
2024-02-15 10:44:02 -0800

    modified url

--- app_setup.py
+++ app_setup.py
@@ -6,6 +6,9 @@ import os
 app = Flask(__name__, static_folder="../frontend/build")
 CORS(app)
 DATABASE_URL = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+index = DATABASE_URL.find("postgres")
+modified_url = DATABASE_URL[:index + len("postgres")] + "ql" + DATABASE_URL[index + len("postgres"):]
+
+app.config['SQLALCHEMY_DATABASE_URI'] = modified_url
 
 db = SQLAlchemy(app)
