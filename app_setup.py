7d2ab2404cf4953a931669daa6ef75ca9c4a575f
princetechn0 <appleluvver8@gmail.com>
2024-02-15 10:17:13 -0800

    env var

--- app_setup.py
+++ app_setup.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__, static_folder="../frontend/build")
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hmnujahcbcycmg:afb438b294cd8aa20fa7b9924b5ca632330cc7693277f5565b70ba60f7a3d6ec@ec2-34-193-110-25.compute-1.amazonaws.com:5432/d6j9pqtj1paevl'
-
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Corrected line
+db_url = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 
 db = SQLAlchemy(app)
